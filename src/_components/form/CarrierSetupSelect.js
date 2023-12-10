fb2263ed3b2c91cb39542a3667b441d5c4eaa6c1
Webster <webster.jorgensen@gmail.com>
2023-12-10 11:25:52 -0600

    carrier setup select

---
+++ src/_components/form/CarrierSetupSelect.js
@@ -0,0 +1,92 @@
+'use client'
+import { Box, Button, Select, Text } from '@chakra-ui/react';
+import { useState } from 'react';
+import InputFloat from './input/inputFloat';
+import SelectWrapper from './input/SelectWrapper';
+import LabelMedium from '../typography/LabelMedium';
+import BodyMedium from '../typography/BodyMedium';
+import { useRouter } from 'next/navigation'
+
+
+export default function CarrierSetupSelect({ data }) {
+
+  const router = useRouter() 
+
+  const [formSubmit, setFormSubmit] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    !formSubmit ? setFormSubmit(true) : setFormSubmit(false)
+
+    const data = {
+      salesRep: e.target.salesRep.value,
+    }
+
+    let baseUrl = 'https://mycarrierpackets.com/76c4b62a-b3ac-40c4-9f31-733d77ee6e09/Carrier/Intellivite/'
+
+    if (data.salesRep) {
+      router.push(baseUrl + data.salesRep)
+    }
+
+  }
+
+  return (
+
+    <Box 
+      mr='0' mt='16'
+      bg={data.attributes.pageColor + '.surface'}
+      p='12' pt='10' 
+      borderRadius='2rem'
+      maxW='4xl'
+    >
+      <Box>
+        <form onSubmit={handleSubmit}>
+          <Box>
+            <Box maxW='md'>
+              <Box mb='2'>
+                <BodyMedium color={data.attributes.pageColor + '.on-surface'}>Select Your Sales Representative</BodyMedium>
+              </Box>
+              <SelectWrapper id={'salesRep'} required={true}>
+                <option value="#">Unknown</option>
+                <option value="alexp2">Alex Perez</option>
+                <option value="andrag">Andra Getty</option>
+                <option value="ajurman">Anna Jurman</option>
+                <option value="asmith">Austin Smith</option>
+                <option value="bwright">Blake Wright</option>
+                <option value="bbrubeck">Brandy Brubeck</option>
+                <option value="brendas">Brenda Simon</option>
+                <option value="bbanach">Brian Banach</option>
+                <option value="bnugent">Bryan Nugent</option>
+                <option value="davida">David Acuna</option>
+                <option value="diegoc">Diego Cardenas</option>
+                <option value="dwright">Drew Wright</option>
+                <option value="danderson">Dustin Anderson</option>
+                <option value="figujua">Felipe Figueredo</option>
+                <option value="fredp">Fred Pauk</option>
+                <option value="jmoore">Jason Moore</option>
+                <option value="jknox">Jeff Knox</option>
+                <option value="jmack">Jeremy Mack</option>
+                <option value="johnl">John Laughlin</option>
+                <option value="jdouty">JR Douty</option>
+                <option value="kennyc">Kenny Copeland</option>
+                <option value="knightingale">Keri Nightingale</option>
+                <option value="kristasl">Krista Schmid-Lee</option>
+                <option value="mhanrahan">Matt Hanrahan</option>
+                <option value="mmeyer">Mike Meyer</option>
+                <option value="nikif">Niki Ferguson</option>
+                <option value="sgardner">Semos Gardner</option>
+                <option value="TylerC">Tyler Cox</option>
+                <option value="zachm">Zach McCoppin</option>
+              </SelectWrapper>
+            </Box>
+          </Box>
+          <Button mt="8" size='lg' variant="primaryDark" type="submit">
+            Let&apos;s Get Started
+          </Button>
+        </form>
+      </Box>
+    </Box> 
+
+  )
+}
--- src/_layout/hero-header/Hero.js
+++ src/_layout/hero-header/Hero.js
@@ -6,6 +6,7 @@ import HeadlineSmall from "@/_components/typography/HeadlineSmall";
 import BodyXl from "@/_components/typography/BodyXl";
 import LabelMedium from "@/_components/typography/LabelMedium";
 import DisplayLarge from "@/_components/typography/DisplayLarge";
+import CarrierSetupSelect from "@/_components/form/CarrierSetupSelect";
 
 
 export default function Hero({ data }) {
@@ -42,14 +43,16 @@ export default function Hero({ data }) {
           <BodyXl color={data?.attributes.pageColor + '.on-surface'} thin>{data?.attributes.Subheading}</BodyXl>
         </Box>
         {
-          data?.attributes.callToAction?.buttonType ? 
+          data?.attributes.callToAction?.buttonType == 'contactShipper' ? 
           <Box mt='12'>
             <Button variant={buttonColor} size='lg'>
-              <Link variant='noDeco' href={buttonUrl}>
+              <Link variant='noDeco' href='/page/contact'>
                 {data?.attributes.callToAction?.buttonText}
               </Link>
             </Button>
           </Box> 
+          : data?.attributes.callToAction?.buttonType == 'contactCarrier' ?
+          <CarrierSetupSelect data={data} />
           : null
         }
       </ContentContainer>
