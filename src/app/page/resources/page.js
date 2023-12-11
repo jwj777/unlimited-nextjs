a09cf5ccb46883fd4cf2f6e331a2dff3f8d9fd3a
Webster <webster.jorgensen@gmail.com>
2023-12-11 11:04:23 -0600

    removed comments from bugged pages

--- src/app/page/resources/page.js
+++ src/app/page/resources/page.js
@@ -15,10 +15,10 @@ export default async function Resources({params}) {
   /* articles */
   const resources = await getPageData();
 
-  console.log('Resources')
-  console.log(resources[0])
-  console.log('Resources - Articles')
-  console.log(data)
+  // console.log('Resources')
+  // console.log(resources[0])
+  // console.log('Resources - Articles')
+  // console.log(data)
   
   return (
     <main>
@@ -76,7 +76,7 @@ async function getArticles() {
   try {
     const response = await fetch(`https://unlimited-strapi-h4fgb.ondigitalocean.app/api/articles?populate[heroImage][populate]=*
     &populate[Sections][populate]=*`, { 
-      next: { revalidate: 30 }
+      next: { revalidate: 60 }
     });
     
     if (!response.ok) {
@@ -98,7 +98,7 @@ async function getArticles() {
 
 async function getPageData() {
   try {
-    const response = await fetch(`https://unlimited-strapi-h4fgb.ondigitalocean.app/api/basic-pages?filters[slug][$eq]=resources`);
+    const response = await fetch(`https://unlimited-strapi-h4fgb.ondigitalocean.app/api/basic-pages?filters[slug][$eq]=resources`, { next: { revalidate: 60 } });
     
     if (!response.ok) {
       throw new Error('Failed to fetch data');
