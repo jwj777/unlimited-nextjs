e2c9e62883930076410fc7d4de59375c48da32c6
Webster <webster.jorgensen@gmail.com>
2023-12-11 13:58:31 -0600

    build fix

--- src/_layout/hero-header/HeroArticle.js
+++ src/_layout/hero-header/HeroArticle.js
@@ -11,7 +11,7 @@ import Image from "next/image";
 
 export default function HeroArticle({ data }) {
 
-  console.log('HeroArticle')
+  // console.log('HeroArticle')
   // console.log(data)
 
   let buttonColor
