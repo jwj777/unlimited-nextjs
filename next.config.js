2fd7dfbaaf807a62528030d2325ff948f0bf0b47
Webster <webster.jorgensen@gmail.com>
2023-12-16 11:26:27 -0600

    redirect fix in next config

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ module.exports = {
   reactStrictMode: true,
   images: {
     loader: "default",
-    domains: ["localhost", 'unlimited-logistics-media.s3.us-east-2.amazonaws.com', 'https://unlimited-logistics-media.s3.us-east-2.amazonaws.com'],
+    domains: ["localhost", 'unlimited-logistics-media.s3.us-east-2.amazonaws.com'],
     minimumCacheTTL: 60,
     remotePatterns: [
       {
@@ -22,95 +22,95 @@ module.exports = {
   async redirects() {
     return [
       {
-        source: '/flatbed/',
+        source: '/flatbed',
         destination: '/service/flatbed',
         permanent: true,
       },
-      // {
-      //   source: '/category/industry-news/',
-      //   destination: '/page/resources',
-      //   permanent: true,
-      // },
-      // {
-      //   source: '/dry/',
-      //   destination: '/service/dry-van',
-      //   permanent: true,
-      // },
-      // {
-      //   source: '/track-shipments/',
-      //   destination: '/page/track-shipments',
-      //   permanent: true,
-      // },
-      // {
-      //   source: '/carriers/',
-      //   destination: '/page/carrier-setup',
-      //   permanent: true,
-      // },
-      // {
-      //   source: '/contact-us/',
-      //   destination: '/page/contact',
-      //   permanent: true,
-      // },
-      // {
-      //   source: '/refrigerated/',
-      //   destination: '/service/refrigerated',
-      //   permanent: true,
-      // },
-      // {
-      //   source: '/request-quote/',
-      //   destination: '/page/contact',
-      //   permanent: true,
-      // },
-      // {
-      //   source: '/industries/',
-      //   destination: '/',
-      //   permanent: true,
-      // },
-      // {
-      //   source: '/news/',
-      //   destination: '/page/resources',
-      //   permanent: true,
-      // },
-      // {
-      //   source: '/category/company-news/',
-      //   destination: '/page/resources',
-      //   permanent: true,
-      // },
-      // {
-      //   source: '/meet-the-team/',
-      //   destination: '/page/about',
-      //   permanent: true,
-      // },
-      // {
-      //   source: '/ltl/',
-      //   destination: '/service/ltl-services',
-      //   permanent: true,
-      // },
-      // {
-      //   source: '/careers/',
-      //   destination: '/page/careers',
-      //   permanent: true,
-      // },
-      // {
-      //   source: '/parcel-analytics/',
-      //   destination: '/service/software',
-      //   permanent: true,
-      // },
-      // {
-      //   source: '/unlimited-in-action/',
-      //   destination: '/page/resources',
-      //   permanent: true,
-      // },
-      // {
-      //   source: '/job-listings/',
-      //   destination: '/page/careers',
-      //   permanent: true,
-      // },     
-      // {
-      //   source: '/joining-the-ranks-of-kansas-citys-fastest-growing-companies/',
-      //   destination: '/page/resources',
-      //   permanent: true,
-      // },
+      {
+        source: '/category/industry-news',
+        destination: '/page/resources',
+        permanent: true,
+      },
+      {
+        source: '/dry',
+        destination: '/service/dry-van',
+        permanent: true,
+      },
+      {
+        source: '/track-shipments',
+        destination: '/page/track-shipments',
+        permanent: true,
+      },
+      {
+        source: '/carriers',
+        destination: '/page/carrier-setup',
+        permanent: true,
+      },
+      {
+        source: '/contact-us',
+        destination: '/page/contact',
+        permanent: true,
+      },
+      {
+        source: '/refrigerated',
+        destination: '/service/refrigerated',
+        permanent: true,
+      },
+      {
+        source: '/request-quote',
+        destination: '/page/contact',
+        permanent: true,
+      },
+      {
+        source: '/industries',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/news',
+        destination: '/page/resources',
+        permanent: true,
+      },
+      {
+        source: '/category/company-news',
+        destination: '/page/resources',
+        permanent: true,
+      },
+      {
+        source: '/meet-the-team',
+        destination: '/page/about',
+        permanent: true,
+      },
+      {
+        source: '/ltl',
+        destination: '/service/ltl-services',
+        permanent: true,
+      },
+      {
+        source: '/careers',
+        destination: '/page/careers',
+        permanent: true,
+      },
+      {
+        source: '/parcel-analytics',
+        destination: '/service/software',
+        permanent: true,
+      },
+      {
+        source: '/unlimited-in-action',
+        destination: '/page/resources',
+        permanent: true,
+      },
+      {
+        source: '/job-listings',
+        destination: '/page/careers',
+        permanent: true,
+      },     
+      {
+        source: '/joining-the-ranks-of-kansas-citys-fastest-growing-companies',
+        destination: '/page/resources',
+        permanent: true,
+      },
     ]
   }
 }
